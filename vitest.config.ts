3088eff5034cb3cef8ef830e20fa10e06f50635d
leaolives <leaolivesoficial@gmail.com>
2026-05-26 13:17:03 -0300

    fix: configure sonar coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+    },
   },
 });
